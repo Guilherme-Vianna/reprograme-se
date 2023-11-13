595264c343bba50f04a0690352e0689dace06ae0
Guilherme Vianna <59426923+Guilherme-Vianna@users.noreply.github.com>
2023-11-13 18:29:30 -0300

    second week dom exercicie added

--- dom semana 2/script.js
+++ dom semana 2/script.js
@@ -3,20 +3,46 @@ var cachorros = [{ "nome": "Thor", "imagem": "dog1" }, { "nome": "Zeca", "imagem
 var gatos = [{ "nome": "Dunga", "imagem": "cat1" }, { "nome": "Mika", "imagem": "cat2" }, { "nome": "Conan", "imagem": "cat3" }, { "nome": "Messi", "imagem": "cat4" }];
 
 // Selecionar elemento 'select_animais' ( 1 ponto)
+var select_animais = document.getElementById("select_animais")
 // Selecionar elemento 'container' ( 1 ponto)
+var container = document.getElementById("container")
 // Selecionar elemento 'nome' ( 1 ponto)
+var nome = document.getElementById("nome")
 
 // Adicionar Listener 'input' ao elemento 'select_animais' (1 ponto)
+select_animais.addEventListener("input", listarAnimais)
 // Adicionar Listener 'mouseover' ao elemento 'container' (1 ponto)
+container.addEventListener("mouseover", mostrarNome)
 // Adicionar Listener 'mouseout' ao elemento 'container' (1 ponto)
+container.addEventListener("mouseout", mostrarNome)
 
 function listarAnimais() { // Implementação da função (10 pontos)
   limparDivContainer();
+  var elemento = document.querySelector("#container");
+
+  if (select_animais.value == "dog") {
+    for (var i = 0; i < cachorros.length; i++) {
+      var imagem = document.createElement("img")
+      imagem.nome = cachorros[i].nome
+      imagem.src = "img/" + cachorros[i].imagem + ".jpg"
+      elemento.appendChild(imagem)
+    }
+  }
+
+  if (select_animais.value == "cat") {
+    for (var i = 0; i < gatos.length; i++) {
+      var imagem = document.createElement("img")
+      imagem.nome = gatos[i].nome
+      imagem.src = "img/" + gatos[i].imagem + ".jpg"
+      elemento.appendChild(imagem)
+    }
+  }
+
   // Implementar...
 }
 
 function mostrarNome(e) { // Implementação da função (4 pontos)
-  // Implementar...
+  nome.innerText = e.target.nome
 }
 
 function limparNome() {
